54ebebb050b0388bbb1b21ac0e834f40bbb05260
Capstone 2020 <capstone2020@tiger>
2020-10-26 17:38:31 -0400

    Remove CMakefiles from git

--- CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/capstone2020/catkin_ws/src/afrl_radar
-# Build directory: /home/capstone2020/catkin_ws/src/afrl_radar
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
