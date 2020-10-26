013c2a021b3cdbdad9636769baa1660ce509b75d
Capstone 2020 <capstone2020@tiger>
2020-10-26 17:37:59 -0400

    Fix whitespace

--- src/radarnode.cpp
+++ src/radarnode.cpp
@@ -10,8 +10,6 @@
 using namespace std;
 
 int main(int argc, char *argv[]) {
-
-  
   ros::init(argc, argv, "radar_node");
   ros::NodeHandle nh;
   ros::Publisher pub = nh.advertise<sensor_msgs::LaserScan> ("scan",10);
@@ -35,23 +33,19 @@ int main(int argc, char *argv[]) {
   ros::Rate loop_rate(20);
 
   while (ros::ok()) {
-
-
     mwRadar.Read(mwr_data,10);
     scan.ranges.resize(mwr_data.size());
     scan.intensities.resize(mwr_data.size());
-    for(int i=0;i<mwr_data.size();++i){
 
+    for (int i=0; i<mwr_data.size(); ++i) {
       scan.header.stamp = ros::Time::now();
       scan.ranges[i] = mwr_data[i].Range;
-
     }
 
-  pub.publish(scan);
-
-  ros::spinOnce();
-  loop_rate.sleep();
-
+    pub.publish(scan);
+    ros::spinOnce();
+    loop_rate.sleep();
   }
+
   return 0;
 }
