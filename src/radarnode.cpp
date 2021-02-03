ead4ade150ff7e46417854e224b225333250f4f1
Capstone 2020 <capstone2020@tiger>
2021-02-03 18:19:43 -0500

    Working version; needs clarification on a few places

--- src/radarnode.cpp
+++ src/radarnode.cpp
@@ -4,15 +4,18 @@
 #include <iostream>
 #include <thread>
 #include <ros/ros.h>
-//#include <pcl_conversions/pcl_conversions.h>
 #include <sensor_msgs/LaserScan.h>
+#include <radar_msgs/RadarDetection.h>
+#include <radar_msgs/RadarDetectionArray.h>
 #include <math.h>
+#include <inttypes.h>
 using namespace std;
 
 int main(int argc, char *argv[]) {
   ros::init(argc, argv, "radar_node");
   ros::NodeHandle nh;
-  ros::Publisher pub = nh.advertise<sensor_msgs::LaserScan> ("scan",10);
+  ros::Publisher laser_pub = nh.advertise<sensor_msgs::LaserScan> ("scan",10);
+  ros::Publisher radar_pub = nh.advertise<radar_msgs::RadarDetectionArray> ("radar",10);
   ros::Rate loop_rate(10);
 
   MWRadar mwRadar;
@@ -31,6 +34,8 @@ int main(int argc, char *argv[]) {
   scan.range_max = 100.0; // meters
   scan.angle_increment = M_PI / 180.0; // radians
 
+  radar_msgs::RadarDetectionArray detections;
+
   scan.ranges.resize(121, NAN);
   scan.intensities.resize(121, NAN);
 
@@ -38,28 +43,41 @@ int main(int argc, char *argv[]) {
     mwRadar.Read(mwr_data,10);
     std::fill(scan.ranges.begin(), scan.ranges.end(), NAN);
     std::fill(scan.intensities.begin(), scan.intensities.end(), NAN);
-    
-    // cout << "size: " << mwr_data.size() << endl;
+    detections.detections.clear();
+
     for (int i=0; i<mwr_data.size(); ++i) {
       MWR_Data current = mwr_data[i];
+      radar_msgs::RadarDetection radar_msg;
       // TODO: this will overwrite data if the angles are too close together
       int index = 120 - (round(current.Azimuth) + 60);
       // TODO: figure out when this is true
       if (index < 0 || 120 < index) continue;
-      
-      // cout << "range[" << i << "] = "
-      //   << "{ range = " << current.Range
-      //   << ", azimuth = " << current.Azimuth
-      //   << ", power = " << current.Power
-      //   << ", index = " << index
-      //   << " }" << endl;
-      
+
+      // record scan message
       scan.ranges[index] = current.Range;
       scan.intensities[index] = current.Power;
-    }
-    scan.header.stamp = ros::Time::now();
 
-    pub.publish(scan);
+      // record radar message
+      // 1. record position, converting polar to cartesian coordinates
+      // TODO: this is inaccurate because of rounding
+      radar_msg.position.x = current.Range * cos(index);
+      radar_msg.position.y = current.Range * sin(index);
+      // 2. record velocity, converting polar to cartesian coordinates
+      radar_msg.velocity.x = current.RadialVelocity * cos(index);
+      radar_msg.velocity.y = current.RadialVelocity * sin(index);
+      // TODO: not sure if these are actually correct
+      radar_msg.amplitude = current.Power;
+      radar_msg.detection_id = current.index;
+
+      detections.detections.push_back(radar_msg);
+    }
+
+    scan.header.stamp = detections.header.stamp = ros::Time::now();
+    detections.header.frame_id = "radar";
+
+    laser_pub.publish(scan);
+    radar_pub.publish(detections);
+
     ros::spinOnce();
     loop_rate.sleep();
   }
